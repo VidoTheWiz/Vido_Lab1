7eb4b93955ab4add726cc2bf04ecc62c326db48d
VidoTheWiz <mcgreg500@morrisville.edu>
2025-09-10 19:52:07 -0400

    Time.time addition

---
+++ Assets/Scripts/Dropper.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    /
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Debug.Log(Time.time);
+    }
+}
